dbc2552cf6b91384b65ae777ed7478194f910f01
Dara Veasna <112950879+daraaveasna@users.noreply.github.com>
2023-10-29 10:12:06 +0700

    TP01 [task01]

---
+++ e-commerce/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {RouterLink, RouterView} from "vue-router";
+</script>
+
+<template>
+  <h2 class="gradient-text">This is my first VueJS project</h2>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+</style>
