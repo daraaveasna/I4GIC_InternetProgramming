31d6820f6ac0343bf93e82ebebc0b6059fa24a3e
Dara Veasna <112950879+daraaveasna@users.noreply.github.com>
2023-11-03 22:00:13 +0700

    update

--- e-commerce/vite.config.js
+++ e-commerce/vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/internet-programming/e-commerce/",
+  base: "/e-commerce/",
   plugins: [vue()],
   resolve: {
     alias: {
