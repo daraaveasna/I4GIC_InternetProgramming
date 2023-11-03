f3a102e0ebdbb7e16e50844e84d7989a1821fbcf
Dara Veasna <112950879+daraaveasna@users.noreply.github.com>
2023-11-03 22:14:58 +0700

    move folder e-commerce

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/e-commerce/",
+  base: "/internet-programming/",
   plugins: [vue()],
   resolve: {
     alias: {
