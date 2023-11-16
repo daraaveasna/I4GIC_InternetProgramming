eb823d168b71a2e8be34fde76bbd7c451f655ee1
Dara Veasna <112950879+daraaveasna@users.noreply.github.com>
2023-11-16 23:22:14 +0700

    update config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/internet-programming/TP02",
+  base: "/internet-programming/",
   plugins: [vue()],
   resolve: {
     alias: {
