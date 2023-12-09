fa4f476f71ce5d34184ba21148c9c496198774df
Dara Veasna <112950879+daraaveasna@users.noreply.github.com>
2023-12-09 08:51:11 +0700

    update Tp04

--- dist/assets/NotFoundView-e8ae8ff9.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as e,c as t,a as n}from"./index-2389a8cd.js";const s={name:"notFoundView"},c=n("h1",null,"Not Found 404",-1),a=[c];function r(_,d,i,u,l,p){return e(),t("div",null,a)}const m=o(s,[["render",r]]);export{m as default};
