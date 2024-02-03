542860a3b62730ecf1155c28eb6b99e5a4bdb81b
Dara Veasna <112950879+daraaveasna@users.noreply.github.com>
2024-02-04 02:00:07 +0700

    Final update TP04

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+
+    public function showLoginForm(){
+        
+    }
+
+    public function register(Request $request){
+
+    }
+
+    public function login(Request $request){
+
+    }
+
+    public function logout(Request $request){
+
+    }
+
+    public function viryfyOTP(Request $request){
+
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        <style>
+            .container{
+                margin: 50px auto;
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <h3>Welcome to Ecommerce Backend server</h3>
+            <h5>
+                Powered by Laravel v.10
+                Version 0.1
+            </h5>
+        </div>
+    </body>
+</html>
